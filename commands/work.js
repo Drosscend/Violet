c8c2272fbf12733bb6c130d49093954ba3d0b66b
Kévin <47970260+Drosscend@users.noreply.github.com>
2019-08-29 15:39:33 +0200

    Create work.js

---
+++ commands/work.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.run = async (client, message, args) => {
+
+
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "work",
+    category: "Economy",
+    description: "Travailler pour gagner des coins.",
+    usage: "work"
+};
